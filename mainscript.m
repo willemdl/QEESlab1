966a1aa623401dc667f2b15c043f0caf28878f6c
Willem de Laat <willem.delaat97@gmail.com>
2020-12-02 15:08:03 +0100

    changes

--- mainscript.m
+++ mainscript.m
@@ -1,1 +1,18 @@
-test;
+%dit is het hoofdscript.
+clear all
+close all
+
+
+Files=dir('Q1_results_rik/transport_time');
+NoFiles= length(Files);
+
+data = zeros(120,NoFiles-3);
+for k=4:NoFiles
+   FileNames = Files(k).name;
+   name(k-3).name = FileNames;
+   data(:,k-3) = importdata(name(k-3).name);
+end
+
+
+boxplot(data);
+
