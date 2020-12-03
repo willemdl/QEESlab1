fd2f0a378cdd6d965bfe4d4dbeee6811a4690e69
willemdl <willem.delaat97@gmail.com>
2020-12-03 15:49:12 +0100

    werkende versie

--- mainscript.m
+++ mainscript.m
@@ -1,48 +1,65 @@
 %dit is het hoofdscript.
 clear all
 close all
-
+%% loading of files 
+%give the folder containing the .txt files
 folder = "Q1_results_rik/transport_time";
 Files=dir(folder);
 NoFiles= length(Files);
+FileOffset = 3; %how many files are in the map that are useless
 
-data = zeros(120,NoFiles-3);
-for k=4:NoFiles
-   FileNames = Files(k).name;
-   name(k-3).name = FileNames;
-   data(:,k-3) = importdata(name(k-3).name);
-   newStr(k-3) = extractBetween(name(k-3).name,"time_","yte");
+info.data = zeros(120,NoFiles-FileOffset);
+multi = 1000; %used to multiply input data 1000 means converting seconds to ms
+for k=1:(NoFiles-FileOffset)
+    FileNames = Files(k+FileOffset).name;
+    info.Fname(k) = convertCharsToStrings(FileNames);%.Fname = File name
+    info.data(:,k) = importdata(info.Fname(k)) *multi;
+end
+%% adjusting data 
+
+expression = '(?<size>\d+)(?<byte>\D+)....';
+for k=1:(NoFiles-FileOffset)
+    temp(k)= regexp(info.Fname(k),expression,'names');
+    info.label(k) = extractBetween(info.Fname(k),"time_","yte");
+    info.size(k) = str2double(temp(k).size);
+    info.byte(k) = temp(k).byte;
+end
+for k=1:(NoFiles-FileOffset)
+    
+    if((contains(info.Fname(k), "Mbyte"))==1)
+        info.rsize(k) = 1000000 * info.size(k);
+    elseif(contains(info.Fname(k),"Kbyte")==1)
+        info.rsize(k) = 1000*info.size(k);
+    elseif(contains(info.Fname(k),"byte")==1)
+        info.rsize(k) =1*info.size(k);
+    else
+        warning("the namefield in size structure was not correct");
+    end
 end
 
-bytes = regexprep(newStr,'[\d"]','');
-bytes = sort(bytes);
-%expression = '_(\d+)(\D+)';
-expression = '(?<size>\d+)(?<byte>\D+)';
-for k=1:15
-size(k) = regexp(name(k).name,expression,'names');
-% if((contains(name(k).name, "Mbyte"))==1)
-%     size(k) = 1000000 * size(k);
-%     
-% elseif(contains(name(k).name,"Kbyte")==1)
-%     
-%     
-%     size(k) = 1000*size(k);
-%     
-% else
-%     size(k) =1*size(k);    
-%     
-% end
-end
-%R = cell2mat(regexp(newStr ,'(?<Name>\D+)(?<Nums>\d+)','names'));
-%tmp = sortrows([{R.Name}' num2cell(cellfun(@(x)str2double(x),{R.Nums}'))]);
-%SortedText = strcat(tmp(:,1) ,cellfun(@(x) num2str(x), tmp(:,2),'unif',0))
-
-data = data*1000;
-test = natsort(newStr); %https://nl.mathworks.com/matlabcentral/answers/229757-sorting-an-array-of-strings-based-on-number-pattern
-
-boxplot(data);
-set(gca,'XTickLabel',newStr);
+figure();
+boxplot(info.data);
+set(gca,'XTickLabel',info.label);
 xlabel('transfersize')
 ylabel('Time [ms]')
 title('Miles per Gallon for All Vehicles')
 
+%% ordering of all info data by size of the packets
+[rsizesorted,ordering] = sort(info.rsize(:));
+info.Fname = info.Fname(ordering);
+info.label = info.label(ordering);
+info.size = info.size(ordering);
+info.byte = info.byte(ordering);
+info.rsize = info.rsize;
+info.data = info.data(:,ordering);
+% test = natsort(newStr); %https://nl.mathworks.com/matlabcentral/answers/229757-sorting-an-array-of-strings-based-on-number-pattern
+
+
+%% boxplot
+figure();
+boxplot(info.data);
+ylim([0 6]);
+set(gca,'XTickLabel',info.label);
+xlabel('Transfersize')
+ylabel('Time [ms]')
+title('Boxplot of the transport time with different packetsizes.')
